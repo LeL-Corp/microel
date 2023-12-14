00c64e6277dc7b89107162f7cf74102632bf05f3
Camilla Devasini <camilladevasini@gmail.com>
2023-12-14 21:10:11 +0100

    remove useless files relative to pages from the repo

--- src/views/routes.js
+++ src/views/routes.js
@@ -2,57 +2,11 @@ import React from 'react';
 
 import {
   IndexView,
-  Home as HomeView,
   Roadmap as RoadmapView,
-  HireUs as HireUsView,
-  Faq as FaqView,
   Settori as SettoriView,
-  CareerListing as CareerListingView,
-  CareerListingMinimal as CareerListingMinimalView,
-  CareerOpening as CareerOpeningView,
-  ContactPage as ContactPageView,
-  Coworking as CoworkingView,
-  Elearning as ElearningView,
-  Enterprise as EnterpriseView,
-  Service as ServiceView,
-  WebBasic as WebBasicView,
-  DesktopApp as DesktopAppView,
-  Expo as ExpoView,
-  Startup as StartupView,
-  DesignCompany as DesignCompanyView,
-  MobileApp as MobileAppView,
-  JobListing as JobListingView,
-  Rental as RentalView,
-  CloudHosting as CloudHostingView,
-  Logistics as LogisticsView,
-  Ecommerce as EcommerceView,
-  Pricing as PricingView,
   About as AboutView,
-  HelpCenter as HelpCenterView,
-  HelpCenterArticle as HelpCenterArticleView,
-  PortfolioPage as PortfolioPageView,
-  PortfolioMasonry as PortfolioMasonryView,
-  PortfolioGrid as PortfolioGridView,
-  CompanyTerms as CompanyTermsView,
   ContactPageSidebarMap as ContactPageSidebarMapView,
   CookiesPolicy as CookiesPolicyView,
-  ContactPageCover as ContactPageCoverView,
-  AboutSideCover as AboutSideCoverView,
-  BlogSearch as BlogSearchView,
-  BlogNewsroom as BlogNewsroomView,
-  BlogArticle as BlogArticleView,
-  BlogReachView as BlogReachViewView,
-  PasswordResetCover as PasswordResetCoverView,
-  PasswordResetSimple as PasswordResetSimpleView,
-  SigninSimple as SigninSimpleView,
-  SigninCover as SigninCoverView,
-  SignupSimple as SignupSimpleView,
-  SignupCover as SignupCoverView,
-  AccountBilling as AccountBillingView,
-  AccountGeneral as AccountGeneralView,
-  AccountNotifications as AccountNotificationsView,
-  AccountSecurity as AccountSecurityView,
-  NotFound as NotFoundView,
   NotFoundCover as NotFoundCoverView,
 } from 'views';
 
@@ -62,125 +16,19 @@ const routes = [
     renderer: (params = {}) => <IndexView {...params} />,
   },
   {
-    path: '/home',
-    renderer: (params = {}) => <HomeView {...params} />,
-  },
-  {
-    path: '/roadmap',
-    renderer: (params = {}) => <RoadmapView {...params} />,
-  },
-  {
-    path: '/hire-us',
-    renderer: (params = {}) => <HireUsView {...params} />,
-  },
-  {
-    path: '/faq',
-    renderer: (params = {}) => <FaqView {...params} />,
-  },
-  {
-    path: '/career-listing',
-    renderer: (params = {}) => <CareerListingView {...params} />,
-  },
-  {
-    path: '/career-listing-minimal',
-    renderer: (params = {}) => <CareerListingMinimalView {...params} />,
-  },
-  {
-    path: '/career-opening',
-    renderer: (params = {}) => <CareerOpeningView {...params} />,
-  },
-  {
-    path: '/contact-page',
-    renderer: (params = {}) => <ContactPageView {...params} />,
-  },
-  {
-    path: '/coworking',
-    renderer: (params = {}) => <CoworkingView {...params} />,
-  },
-  {
-    path: '/e-learning',
-    renderer: (params = {}) => <ElearningView {...params} />,
-  },
-  {
-    path: '/enterprise',
-    renderer: (params = {}) => <EnterpriseView {...params} />,
-  },
-  {
-    path: '/service',
-    renderer: (params = {}) => <ServiceView {...params} />,
-  },
-  {
-    path: '/web-basic',
-    renderer: (params = {}) => <WebBasicView {...params} />,
-  },
-  {
-    path: '/desktop-app',
-    renderer: (params = {}) => <DesktopAppView {...params} />,
-  },
-  {
-    path: '/expo',
-    renderer: (params = {}) => <ExpoView {...params} />,
+    path: '/about',
+    renderer: (params = {}) => <AboutView {...params} />,
   },
   {
     path: '/areas',
     renderer: (params = {}) => <SettoriView {...params} />,
   },
+
   {
-    path: '/startup',
-    renderer: (params = {}) => <StartupView {...params} />,
+    path: '/roadmap',
+    renderer: (params = {}) => <RoadmapView {...params} />,
   },
-  {
-    path: '/design-company',
-    renderer: (params = {}) => <DesignCompanyView {...params} />,
-  },
-  {
-    path: '/mobile-app',
-    renderer: (params = {}) => <MobileAppView {...params} />,
-  },
-  {
-    path: '/job-listing',
-    renderer: (params = {}) => <JobListingView {...params} />,
-  },
-  {
-    path: '/rental',
-    renderer: (params = {}) => <RentalView {...params} />,
-  },
-  {
-    path: '/cloud-hosting',
-    renderer: (params = {}) => <CloudHostingView {...params} />,
-  },
-  {
-    path: '/logistics',
-    renderer: (params = {}) => <LogisticsView {...params} />,
-  },
-  {
-    path: '/e-commerce',
-    renderer: (params = {}) => <EcommerceView {...params} />,
-  },
-  {
-    path: '/help-center',
-    renderer: (params = {}) => <HelpCenterView {...params} />,
-  },
-  {
-    path: '/help-center-article',
-    renderer: (params = {}) => <HelpCenterArticleView {...params} />,
-  },
-  {
-    path: '/portfolio-page',
-    renderer: (params = {}) => <PortfolioPageView {...params} />,
-  },
-  {
-    path: '/portfolio-masonry',
-    renderer: (params = {}) => <PortfolioMasonryView {...params} />,
-  },
-  {
-    path: '/portfolio-grid',
-    renderer: (params = {}) => <PortfolioGridView {...params} />,
-  },
-  {
-    path: '/company-terms',
-    renderer: (params = {}) => <CompanyTermsView {...params} />,
-  },
+
   {
     path: '/contacts',
     renderer: (params = {}) => <ContactPageSidebarMapView {...params} />,
@@ -193,84 +41,9 @@ const routes = [
     path: '/cookies-policy',
     renderer: (params = {}) => <CookiesPolicyView {...params} />,
   },
-  {
-    path: '/contact-page-cover',
-    renderer: (params = {}) => <ContactPageCoverView {...params} />,
-  },
-  {
-    path: '/about',
-    renderer: (params = {}) => <AboutView {...params} />,
-  },
-  {
-    path: '/about-side-cover',
-    renderer: (params = {}) => <AboutSideCoverView {...params} />,
-  },
-  {
-    path: '/pricing',
-    renderer: (params = {}) => <PricingView {...params} />,
-  },
-  {
-    path: '/blog-search',
-    renderer: (params = {}) => <BlogSearchView {...params} />,
-  },
-  {
-    path: '/blog-newsroom',
-    renderer: (params = {}) => <BlogNewsroomView {...params} />,
-  },
-  {
-    path: '/blog-article',
-    renderer: (params = {}) => <BlogArticleView {...params} />,
-  },
-  {
-    path: '/blog-reach-view',
-    renderer: (params = {}) => <BlogReachViewView {...params} />,
-  },
-  {
-    path: '/password-reset-cover',
-    renderer: (params = {}) => <PasswordResetCoverView {...params} />,
-  },
-  {
-    path: '/password-reset-simple',
-    renderer: (params = {}) => <PasswordResetSimpleView {...params} />,
-  },
-  {
-    path: '/signin-simple',
-    renderer: (params = {}) => <SigninSimpleView {...params} />,
-  },
-  {
-    path: '/signin-cover',
-    renderer: (params = {}) => <SigninCoverView {...params} />,
-  },
-  {
-    path: '/signup-simple',
-    renderer: (params = {}) => <SignupSimpleView {...params} />,
-  },
-  {
-    path: '/signup-cover',
-    renderer: (params = {}) => <SignupCoverView {...params} />,
-  },
-  {
-    path: '/account-billing',
-    renderer: (params = {}) => <AccountBillingView {...params} />,
-  },
-  {
-    path: '/account-general',
-    renderer: (params = {}) => <AccountGeneralView {...params} />,
-  },
-  {
-    path: '/account-notifications',
-    renderer: (params = {}) => <AccountNotificationsView {...params} />,
-  },
-  {
-    path: '/account-security',
-    renderer: (params = {}) => <AccountSecurityView {...params} />,
-  },
+
   {
     path: '/not-found',
-    renderer: (params = {}) => <NotFoundView {...params} />,
-  },
-  {
-    path: '/not-found-cover',
     renderer: (params = {}) => <NotFoundCoverView {...params} />,
   },
 ];
